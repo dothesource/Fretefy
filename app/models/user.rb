25d4022d0ec60f86bed86285284877292032e444
André Schilipack <andre.schilipack@gmail.com>
2018-05-27 23:09:31 -0300

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
+
+  after_create :create_account
+
+  private
+
+  def create_account
+    Account.create(user_id: self.id)
+  end
+end
